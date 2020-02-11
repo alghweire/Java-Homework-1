b45812c4f0c042a85f64c58fd25d24ca6ba0aaaf
alghweire <60858707+alghweire@users.noreply.github.com>
2020-02-10 21:25:36 -0800

    updated homework 1

--- src/com/testpro/hw1/Main.java
+++ src/com/testpro/hw1/Main.java
@@ -8,43 +8,33 @@ public class Main {
     public static void main(String[] args) {
 
 
-// Question 3: Call index outside of the bound. Handle this exception printing “Outside of bound message.”
 
-        try {
-            //  Block of code to try
-            int [] arr = {1,2,3};
-            System.out.println(arr[4]);
-        }
-        catch(Exception e) {
-            //  Block of code to handle errors
-            System.out.println("Outside of bound message.");
-        }
-
-
-// answer for question 2
-        System.out.println(array[1]);
-
-    }
-
-    /* ------------------------------------------------------------ */
-                                /* HW1#A
-         /*create a loop that wil print out numbers 1- 10 except #7
+// Question 1: create a loop that wil print out numbers 1- 10 except #7
         for (int i = 0; i <= 10; i++) {
-           if ( i != 7 ) {
+            if (i != 7) {
                 System.out.println(i);
-                  continue;
-           }
-         /* ------------------------------------------------------------- */
-
+                continue;
+            }
 
 // Question 2: Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
-
-
-    static int[] array = {1, 2, 3, 4, 5};
-
-
+        int[] array = {1, 2, 3, 4, 5};
+            System.out.println(array[1]);
 
 
 
+// Question 3: Call index outside of the bound. Handle this exception printing “Outside of bound message.”
 
+            try {
+                //  Block of code to try
+                int[] arr = {1, 2, 3};
+                System.out.println(arr[4]);
+            } catch (Exception e) {
+                //  Block of code to handle errors
+                System.out.println("Outside of bound message.");
+            }
+
+            }
+
+
+    }
 }
