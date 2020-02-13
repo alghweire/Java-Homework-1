240b65f00400b986e4c0dfc2bd57b6232c2096a6
alghweire <60858707+alghweire@users.noreply.github.com>
2020-02-12 19:24:37 -0800

    Hw3 complete

--- src/com/testpro/hw3/HW3.java
+++ src/com/testpro/hw3/HW3.java
@@ -1,4 +1,36 @@
-package com.testpro.hw3;
+package com.testpro.hw3 ;
+
+import java.util.Scanner;
 
 public class HW3 {
+
+    // Was it a cat I saw // Create a method that will check if a given string is a palindrome. If it is it should print
+    // “This string is a palindrome” if not print “This string is not a palindrome”
+    //  Upload it to Github and share a link in Github with your exercise
+
+    public static void main (String args[]) {
+
+        Scanner a = new Scanner(System.in);
+        System.out.println(" lets verify if your word or sentence is a Pelindrom ") ;
+        String input = a.nextLine();
+        String reverse = "";
+
+        for ( int i = input.length() -1; i>= 0 ; i-- ) {
+            reverse = reverse + input.charAt(i);
+        }
+            if (input.equals(reverse)) {
+
+                System.out.println(" this is a pelindrom ");
+            }
+            else {
+                System.out.println("this is not a pelindrom ");
+
+            }
+
+
+
+
+
+    }
+
 }
