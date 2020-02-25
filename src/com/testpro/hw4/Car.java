3084f6b0c0b2564d5dc74e4cc8f489d86c6ff4db
alghweire <60858707+alghweire@users.noreply.github.com>
2020-02-24 20:33:19 -0800

    homework week3 part 2 complete

--- src/com/testpro/hw4/Car.java
+++ src/com/testpro/hw4/Car.java
@@ -1,8 +1,62 @@
 package com.testpro.hw4;
 
-public class Car {
+class Car {
 
-    public static void main(String[] args){
+
+
+    private String Country_Of_Car;
+    private String model;
+    private String make;
+
+
+    public String getCountry_Of_Car() {
+        return Country_Of_Car ;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake (){
+        return make;
+    }
+
+    public void setCountry_Of_car(String c){
+        this.Country_Of_Car = c;
 
     }
+    public void setModel(String m){
+        this.model = m;
+    }
+    public void setMake(String maker){
+        this.make = maker;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Car audi = new Car();
+        Car bmw = new Car();
+        Car mercedes = new Car();
+
+        audi.setCountry_Of_car("Germany");
+        audi.setMake("Audi");
+        audi.setModel("rs7");
+
+        bmw.setCountry_Of_car("Germany");
+        bmw.setMake("bmw");
+        bmw.setModel("M4");
+
+        mercedes.setCountry_Of_car("Germany");
+        mercedes.setMake("Mercedes");
+        mercedes.setModel("S550");
+
+        System.out.println("Country of Car: " + audi.getCountry_Of_Car() + "\n "  +  "Car Make :  " + audi.getMake() + "\n " +  "Car Model: " + audi.getModel() +" \n ");
+
+        System.out.println("Country of Car: " + bmw.getCountry_Of_Car() + "\n "  +  "Car Make :  " + bmw.getMake() + " \n " +  "Car Model: " + bmw.getModel() + " \n ");
+
+        System.out.println("Country of Car: " + mercedes.getCountry_Of_Car() + "\n "  +  "Car Make :  " + mercedes.getMake() + " \n " +  "Car Model: " + mercedes.getModel()+" \n ");
+    }
+
 }
